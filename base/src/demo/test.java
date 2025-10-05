fab7734229b861f8cdd9ba7478080f33dcb0bd8c
dashstar22 <xubocheng22@gmail.com>
2025-10-06 01:13:49 +0800

    bbb

--- base/src/demo/test.java
+++ base/src/demo/test.java
@@ -11,7 +11,7 @@ public class test {
         Pay(c2);
     }
     public static void Pay(Card c) {
-        System.out.println("请输入金额：jjj");
+        System.out.println("请输入金额：bbb");
         Scanner sc = new Scanner(System.in);
         double money = sc.nextDouble();
         c.pay(money);
